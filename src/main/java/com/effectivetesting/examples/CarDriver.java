636e291d91e27d32a6db28c94db8fbcc4a8af844
marcelo corpucci <mcorpucci@gmail.com>
2018-08-14 16:47:49 -0700

    Ejercicio 6 completo

--- src/main/java/com/effectivetesting/examples/CarDriver.java
+++ src/main/java/com/effectivetesting/examples/CarDriver.java
@@ -3,6 +3,8 @@ package com.effectivetesting.examples;
 public class CarDriver {
 	public static void main(String[] args) {
 		Car myCar = new Car();
+		
+		System.out.println("Tire sensor meassurement: " + myCar.getTireSensorCriteria());
 		myCar.startEngine();
 		System.out.println("Current rpms: " + myCar.getCurrentRmp());
 		
---
+++ src/main/java/com/effectivetesting/examples/Sensor.java
@@ -0,0 +1,15 @@
+package com.effectivetesting.examples;
+
+public class Sensor {
+private String meassurementUnit;
+
+	public Sensor(String meassurementUnit) {
+		this.meassurementUnit = meassurementUnit;
+}
+	public String getMeassurementUnit() {
+		return this.meassurementUnit;
+		
+	}
+
+}
+
