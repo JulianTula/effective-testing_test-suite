5db90000440df9f1752c30f679b84220da059af1
marcelo corpucci <mcorpucci@gmail.com>
2018-08-16 21:54:33 -0700

    Ejercicio 7 resuelto

--- src/test/java/com/effectivetesting/entry/TestCreateEntry.java
+++ src/test/java/com/effectivetesting/entry/TestCreateEntry.java
@@ -7,9 +7,12 @@ import java.util.concurrent.TimeUnit;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestCreateEntry {
 	private WebDriver driver;
@@ -22,9 +25,12 @@ public class TestCreateEntry {
 		driver.findElement(By.id("btn-submit")).click();
 		
 		driver.findElement(By.id("create_post")).click();
-		driver.findElement(By.id("title")).sendKeys("My newest post");
+		driver.findElement(By.id("title")).sendKeys("My newest post6");
 		driver.findElement(By.id("body")).sendKeys("This is a post.");
 		driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[1]/form/div[5]/div/button")).click();
+		
+//		WebDriverWait wait = new WebDriverWait(driver, 20);
+//		wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div[2]/div[1]/div[1]/div/span")));
 		
 		String currentMessage = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div[1]/div/span")).getText();
 		System.out.println(currentMessage);
@@ -36,12 +42,23 @@ public class TestCreateEntry {
 	public void setUp() {
 		System.setProperty("webdriver.chrome.driver", "C:\\ChromeDriver\\chromedriver.exe");
 		driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(15, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(25, TimeUnit.SECONDS);
 		driver.get("localhost:5000");
 	}
 
 	@After
 	public void teardDown() {
+  		driver.get("http://localhost:5000/admin/entry/");
+  		driver.findElement(By.xpath("/html/body/div/table/tbody/tr[2]/td[2]/form/button/i")).click();
+  		
+ 		WebDriverWait wait = new WebDriverWait(driver, 10);
+ 		Alert alert = wait.until(ExpectedConditions.alertIsPresent());
+ 		
+ 		alert.accept();
+ 		
+ 		WebDriverWait waitForMessage = new WebDriverWait(driver, 10);
+ 		waitForMessage.until(ExpectedConditions.presenceOfElementLocated(By.xpath("/html/body/div/table/tbody/tr[2]/td[2]/form/button/i")));
+		
 		driver.quit();
 	}
 }
